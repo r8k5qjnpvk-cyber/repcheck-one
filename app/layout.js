934ef1a37d001d949599a424c9df7e37525b4538
Cal Finnegan <calfinnegan@Cals-iMac.local>
2026-03-03 15:09:07 +0000

    Update AdSense ID to ca-pub-7175000491834186

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <head>
         <script
           async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR_ADSENSE_ID"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7175000491834186"
           crossOrigin="anonymous"
         />
       </head>
